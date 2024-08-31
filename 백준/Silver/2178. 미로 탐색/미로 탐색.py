37d4c3d506a93403d7eb3bc3cc8f2882ae3988b2
soyoung2118 <80445035+soyoung2118@users.noreply.github.com>
2024-08-31 15:13:13 +0900

    [Silver I] Title: 미로 탐색, Time: 68 ms, Memory: 34044 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -0,0 +1,30 @@
+from collections import deque;
+
+N, M = map(int, input().split())
+
+graph = []
+
+for _ in range(N):
+    graph.append(list(map(int, input())))
+
+
+def bfs(x, y):
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
+
+    queue = deque()
+    queue.append((x,y))
+
+    while queue:
+        x,y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0<=nx<N and 0<=ny<M and graph[nx][ny]==1:
+                graph[nx][ny] = graph[x][y]+1
+                queue.append((nx,ny))
+
+    return graph[N-1][M-1]
+
+print(bfs(0,0))
