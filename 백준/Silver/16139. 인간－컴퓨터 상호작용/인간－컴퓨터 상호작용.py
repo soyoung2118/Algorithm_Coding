c99d6ee42c16a5899bfc923f2fd787d69517f16c
soyoung2118 <80445035+soyoung2118@users.noreply.github.com>
2024-11-10 18:51:35 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 124 ms, Memory: 110972 KB, Score: 50 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -2,10 +2,16 @@ import sys
 input = sys.stdin.readline
 
 s = input()
+
+arr = [[0]*len(s) for _  in range(26)]
+for i in range(ord('a'),ord('z')+1):
+  for j in range(len(s)):
+    arr[i-97][j] = s.count(chr(i), 0, j)
+
 q= int(input())
 
 for _ in range(q):
   a, l, r = input().split()
   l, r = map(int, [l,r])
-  cnt = s.count(a, l, r+1)
+  cnt = arr[ord(a)-97][r+1] - arr[ord(a)-97][l] 
   print(cnt)
