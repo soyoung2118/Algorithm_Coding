59db49100702e2004ddbec44c7af694a40406262
soyoung2118 <80445035+soyoung2118@users.noreply.github.com>
2024-12-29 19:14:30 +0900

    [Silver II] Title: 종이의 개수, Time: 2360 ms, Memory: 71292 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.py
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.py
@@ -1,3 +1,6 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 
 lst = []
@@ -30,7 +33,6 @@ def check(x_start, y_start, length):
                 break
         if not same:
             break
-
     if same:
         append_result(sval)
     else:
