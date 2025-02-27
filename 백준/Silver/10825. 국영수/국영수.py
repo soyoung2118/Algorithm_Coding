5c3041d68606706249bd5fd77ab71588db8d55a4
soyoung2118 <80445035+soyoung2118@users.noreply.github.com>
2025-02-27 16:20:24 +0900

    [Silver IV] Title: 국영수, Time: 456 ms, Memory: 66264 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.py
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -1,16 +1,15 @@
-N = int(input())
+import sys
+input = sys.stdin.readline
 
+N = int(input())
 students = []
 
 for _ in range(N):
   student = input().split()
-
   name = student[0]  
   scores = list(map(int, student[1:])) 
-
   student = [name] + scores
   students.append(student)
-
 
 students = sorted(students, key=lambda x: (-x[1], x[2], -x[3], x[0]))
 
