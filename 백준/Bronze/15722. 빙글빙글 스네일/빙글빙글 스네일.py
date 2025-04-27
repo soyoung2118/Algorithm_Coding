a90850d23d0717a82bd8e4d85720772f665cd084
soyoung2118 <80445035+soyoung2118@users.noreply.github.com>
2025-04-27 20:54:39 +0900

    [Bronze I] Title: 빙글빙글 스네일, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/15722. 빙글빙글 스네일/빙글빙글 스네일.py
+++ 백준/Bronze/15722. 빙글빙글 스네일/빙글빙글 스네일.py
@@ -1,25 +1,23 @@
 n = int(input())
-
 x = 0
 y = 0
-step = 1     # 현재 이동 거리
-moved = 0    # 현재까지 이동한 횟수
+step = 1    
+time = 0    
 dir = 0
 
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
 
-while moved < n:
+while time < n:
     for _ in range(step):
-        if moved == n:
+        if time == n:
             break
         x += dx[dir]
         y += dy[dir]
-        moved += 1
+        time += 1
 
-    dir = (dir + 1) % 4  # 시계 방향 전환
+    dir = (dir + 1) % 4  
 
-    # 방향 2번마다 step 증가
     if dir % 2 == 0:
         step += 1
 
