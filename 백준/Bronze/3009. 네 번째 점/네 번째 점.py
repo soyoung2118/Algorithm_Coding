29a54c1e72f8072f7c24f79c60582ce6fbd5e66d
soyoung2118 <80445035+soyoung2118@users.noreply.github.com>
2026-01-11 20:26:35 +0900

    [Bronze III] Title: 네 번째 점, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -0,0 +1,15 @@
+x_lst = []
+y_lst = []
+
+for _ in range(3):
+  x,y = map(int, input().split())
+  x_lst.append(x)
+  y_lst.append(y)
+
+for i in range(3):
+  if x_lst.count(x_lst[i]) == 1:
+    x = x_lst[i]
+  if y_lst.count(y_lst[i]) == 1:
+    y = y_lst[i]
+
+print(x,y)
